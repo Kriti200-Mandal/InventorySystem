c4c509b8dc258201e61b61430bb9e46ad4aef952
Kriti200-Mandal <kritimandal115@gmail.com>
2025-12-26 16:09:36 +0545

    Modify All

--- src/Model/InventoryModel.java
+++ src/Model/InventoryModel.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model;
-import java.util.HashMap;
+import java.util.Stack;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  *
@@ -11,13 +13,80 @@ import java.util.HashMap;
  */
 public class InventoryModel {
     
-    private final HashMap <String , item> Item;
-    
-    
-    public InventoryModel()
-    {
-        Item = new HashMap<>();
+   private ArrayList<item>Items;
+   private Stack<item> recentItems;
+   
+        public InventoryModel()
+        {
+            Items = new ArrayList<>();
+            recentItems =  new Stack<>();
+            
+            
+        }
+        public boolean addItem(item items)
+        {
+            for(item i : Items)
+            {
+                if (i.getProductId().equalsIgnoreCase(items.getProductId())) 
+  
+                {
+                    return false;
+                }
+            }
+            Items.add(items);
+            recentItems.push(items);
+            return true;
+            
+        }
+        public ArrayList<item> getAllItems()
+        {
+            return Items;
+        }
+          public boolean deleteItem(String produtId)
+        {
+            for(item i : Items)
+            {
+                if(i.getProductId().equalsIgnoreCase(produtId))
+                {
+                    Items.remove(i);
+                      return true;
+                }
+                  
+                }
+                return false;
+            }
+          public ArrayList<item> getLowStockItem()
+          {
+              ArrayList<item> lowStock = new ArrayList<>();
+              for (item i : Items)
+              {
+                  if (i.getQuantity() <= i.getMinimum())
+                  {
+                      lowStock.add(i);
+                  }
+              }
+              return lowStock;
+          } 
+          public item getLastAddedItem()
+          {
+              if(recentItems.isEmpty())
+              {
+                  return null;
+              }
+              return recentItems.peek();
+          }
+         public boolean updateItem(String productId, int newQuantity, int min, double newPrice) {
+    for (item i : Items) {
+        if (i.getProductId().equalsIgnoreCase(productId)) {
+            i.setQuantity(newQuantity);
+            i.setMinimum(min);
+            i.setPrice(newPrice);
+            return true;
+        }
     }
-    
-    
+    return false;
+}
+          
+        
+        
 }
